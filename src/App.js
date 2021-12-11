79740c2c52f9fb0de10957df1a9fecb2a50b3649
Mounika Ravi Hospet <mounikaravi.hospet@gmail.com>
2021-12-11 13:09:10 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1>
+        Youtube Clone
+      </h1> */}
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      {/* Recommended Videos */}
+    </div>
+  );
+}
+
+export default App;
